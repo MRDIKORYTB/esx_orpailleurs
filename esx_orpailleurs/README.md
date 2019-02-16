d456768f13e74d13a9c1a82b99c9c2ca581b8751
MR_DIKOR_YTB <gamer11360@gmail.com>
2019-02-16 03:35:20 +0100

    Add files via upload

---
+++ esx_orpailleurs/README.md
@@ -0,0 +1,22 @@
+# fxserver-esx_orpailleurs
+FXServer ESX orpailleurs
+
+If you want to modify or add somethings to the script please contact us or mention us
+[REQUIREMENTS]
+
+	* Auto mode
+	* esx_service => https://github.com/FXServer-ESX/fxserver-esx_service
+  
+	* Player management (billing and boss actions)
+	* esx_society => https://github.com/FXServer-ESX/fxserver-esx_society
+	* esx_billing => https://github.com/FXServer-ESX/fxserver-esx_billing
+
+[INSTALLATION]
+
+1) CD in your resources/[esx] folder
+2) Copy the repository
+3) Import esx_orpailleurs.sql in your database (not the both)
+
+4) Add this in your server.cfg :
+
+start esx_orpailleurs
---
+++ esx_orpailleurs/config.lua
@@ -0,0 +1,68 @@
+Config                            = {}
+Config.DrawDistance               = 100.0
+Config.MaxInService               = -1
+Config.EnablePlayerManagement     = true
+Config.EnableSocietyOwnedVehicles = false
+Config.Locale                     = 'fr'
+
+Config.Zones = {
+
+	PepiteDorFarm = {
+		Pos   = {x = 2837.804, y = -657.504, z = 1.007},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Récolte de pepite d'or",
+		Type  = 1
+	},
+
+
+	TraitementLingotDor = {
+		Pos   = {x = 1114.376, y = -2006.653, z = 35.439},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Traitement de lingot d'or",
+		Type  = 1
+	},
+
+	SellFarm = {
+		Pos   = {x = -96.328, y = -806.523, z = 44.038},
+		Size  = {x = 3.5, y = 3.5, z = 2.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Vente des lingots d'or",
+		Type  = 1
+	},
+
+	OrpailleursActions = {
+		Pos   = {x = 1392.126, y = 1153.167, z = 114.443},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Point d'action",
+		Type  = 0
+	 },
+	  
+	VehicleSpawner = {
+		Pos   = {x = 1373.134, y = 1152.035, z = 113.759},
+		Size = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Garage véhicule",
+		Type  = 0
+	},
+
+	VehicleSpawnPoint = {
+		Pos   = {x = 1368.604, y = 1151.556, z = 113.759},
+		Size  = {x = 1.5, y = 1.5, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Spawn point",
+		Type  = -1
+	},
+
+	VehicleDeleter = {
+		Pos   = {x = 1360.814, y = 1140.007, z = 113.759},
+		Size  = {x = 3.0, y = 3.0, z = 1.0},
+		Color = {r = 136, g = 243, b = 216},
+		Name  = "Ranger son véhicule",
+		Type  = 0
+	}
+
+}
+
